e38a74957bcdf1590f3ce46173159ed95959d801
fenggw <fenggw@asiainfo.com>
2015-07-09 15:53:14 +0800

    test

--- src/test/java/test/com/ai/paas/ipaas/ccs/configclient/ConcurrentTest.java
+++ src/test/java/test/com/ai/paas/ipaas/ccs/configclient/ConcurrentTest.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.zookeeper.CreateMode;
+
+import test.com.ai.paas.ipaas.ccs.impl.util.ConfigSDKUtil;
+
 import com.ai.paas.ipaas.ccs.ConfigFactory;
 import com.ai.paas.ipaas.ccs.IConfigClient;
 import com.ai.paas.ipaas.ccs.constants.ConfigException;
+import com.ai.paas.ipaas.ccs.zookeeper.ZKClient;
+import com.ai.paas.ipaas.ccs.zookeeper.impl.ZKPoolFactory;
 import com.ai.paas.ipaas.uac.vo.AuthDescriptor;
+import com.ai.paas.ipaas.util.UUIDTool;
 
 public class ConcurrentTest {
 	
@@ -21,6 +28,11 @@ public class ConcurrentTest {
 	private static final String SERVICE_ID = "CCS001";
 	private static AuthDescriptor ad = null;
 	private static IConfigClient configClient;
+	
+    private static String userPwd = "123456";
+    private final static String configAddr = "10.1.228.198:39182";
+    private static String adminName = "admin";
+    private static String adminPwd = "ec4c9e0e78f76a69";
 	
 	static {
 		ad = new AuthDescriptor(URL, USER_NAME, PASSWORD, SERVICE_ID);
@@ -49,25 +61,25 @@ public class ConcurrentTest {
 			final AtomicInteger ind = new AtomicInteger();
 			Thread hj = new Thread(new Runnable() {
 				public void run() {
-					IConfigClient configClient= null;
+					IConfigClient configClient;
 					try {
-						configClient = ConfigFactory.getConfigClient(ad);
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-					for (int j = 0; (j = ind.getAndIncrement()) < times;) {
-						final String key = "/"+k+"----"+j;
-						try {
-							switch(type){
-								case "add" : configClient.add(key,"");break;
-								case "delete" : configClient.remove(key);;break;
-								case "modify" : configClient.modify(key,"123");break;
-								case "get" : configClient.get(key);break;
-								default : System.out.println("error");
+						configClient = getIConfigClient();
+						for (int j = 0; (j = ind.getAndIncrement()) < times;) {
+							final String key = "/"+k+"----"+j;
+							try {
+								switch(type){
+									case "add" : configClient.add(key,"");break;
+									case "delete" : configClient.remove(key);;break;
+									case "modify" : configClient.modify(key,"123");break;
+									case "get" : configClient.get(key);break;
+									default : System.out.println("error");
+								}
+							} catch (ConfigException e) {
+								e.printStackTrace();
 							}
-						} catch (ConfigException e) {
-							e.printStackTrace();
 						}
+					}  catch (Exception e) {
+						e.printStackTrace();
 					}
 				}
 			});
@@ -78,75 +90,17 @@ public class ConcurrentTest {
 			t.join();
 	}
 
-
-
-//	public static void main(String[] args) throws Exception {
-//		
-//		String userName = "371308601@qq.com";
-//		
-//		String userPwd = "123456";
-//		
-//		String AUTH_ADDR = "http://10.1.228.198:14821/iPaas-Auth/service/check";
-//		
-//		AuthDescriptor ad = new AuthDescriptor(AUTH_ADDR, userName, userPwd, "CCS001"); 
-//		
-//		final IConfigClient iConfigClient = ConfigFactory.getConfigClient(ad);
-//
-//		int thread_num = 1000;
-//		runTestCase(iConfigClient,99);
-//
-//		List<Thread> threadPool = new ArrayList<Thread>();
-//		for (int index = 0; index < thread_num; index++) {
-//			
-//			final int NO = index + 100;
-//			Runnable run = new Runnable() {
-//				public void run() {
-//					try {
-//						runTestCase(iConfigClient,NO);
-//						// 业务逻辑
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			};
-//			Thread testThread = new Thread(run);
-//			threadPool.add(testThread);
-//		}
-//		
-//		thread_num = Integer.parseInt(args[0]);
-//		times = Integer.parseInt(args[1]);
-//		List<Thread> threadPool = new ArrayList<Thread>();
-//		
-//		long t = System.currentTimeMillis();
-//		
-//		for (Thread testThread : threadPool) {
-//			testThread.start();
-//		}
-//		long elapsed = System.currentTimeMillis() - t;
-//		System.out.println(elapsed+"---------"+((1000 * times) / elapsed) + " ops");
-//
-//		Thread.sleep(1000000L);
-//		// runTestCase(ds,101);
-//	}
-
-	public static void runTestCase(IConfigClient iConfigClient,int NO)
-			throws Exception {
-		testAdd(iConfigClient,NO);
-	}
-	
-	public static void testAdd(IConfigClient iConfigClient,int NO) throws ConfigException{
-		iConfigClient.add("/abc"+NO, "abcdefghijklmnopqrstuvwxyz");
-	}
-	
-	public static void testDelete(IConfigClient iConfigClient,int NO) throws ConfigException{
-		iConfigClient.remove("/abc"+NO);
-	}
-	
-	public static void testGet(IConfigClient iConfigClient,int NO) throws ConfigException{
-		iConfigClient.get("/abc"+NO);
-	}
-	
-	public static void testModify(IConfigClient iConfigClient,int NO) throws ConfigException{
-		iConfigClient.modify("/abc"+NO, "test modified");
+	public static IConfigClient getIConfigClient() throws ConfigException, Exception{
+		String userName = UUIDTool.genId32();
+		ZKClient client = ZKPoolFactory.getZKPool(configAddr, adminName, adminPwd).getZkClient(configAddr, adminName);
+	    client.createNode(ConfigSDKUtil.appendUserNodePath(userName, SERVICE_ID), ConfigSDKUtil.createWritableACL(userName,
+	            String.valueOf(userPwd), adminName, adminPwd), "test value", CreateMode.PERSISTENT);
+	    
+	    AuthDescriptor descriptor = new AuthDescriptor();
+        descriptor.setServiceId(SERVICE_ID);
+        descriptor.setUserName(userName);
+        descriptor.setAuthAdress("http://10.1.228.198:14821/iPaas-Auth/service/check");
+        descriptor.setPassword(userPwd);
+        return ConfigFactory.getConfigClient(descriptor);
 	}
 }
